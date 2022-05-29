15c7539138fa196e96aa2454e9ae35116203d7b3
JeremyBrakha <jrmbrakha@gmail.com>
2022-05-29 23:10:23 +0200

    Implémentation méthodes

--- Kruskal/src/liste/listeChaineeTeteQueue/ListeChaineeTQ.java
+++ Kruskal/src/liste/listeChaineeTeteQueue/ListeChaineeTQ.java
@@ -1,5 +1,24 @@
 package liste.listeChaineeTeteQueue;
 
 public class ListeChaineeTQ {
-    
+    private Noeud tete;
+    private Noeud queue;
+
+    public ListeChaineeTQ(Noeud tete) {
+        this.tete = tete;
+        this.queue = null;
+    }
+
+    public ListeChaineeTQ(Noeud tete, Noeud queue) {
+        this.tete = tete;
+        this.queue = queue;
+    }
+
+    public boolean teteEstVide() {
+        return this.tete == null;
+    }
+
+    public boolean queueEstVide() {
+        return this.queue == null;
+    }
 }
