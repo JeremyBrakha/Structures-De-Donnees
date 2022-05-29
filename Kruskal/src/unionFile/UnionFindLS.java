7c6d504c391b48f8178ac8268fc421fd4349a8cb
JeremyBrakha <jrmbrakha@gmail.com>
2022-05-29 12:03:11 +0200

    maj

--- Kruskal/src/unionFile/UnionFindLS.java
+++ Kruskal/src/unionFile/UnionFindLS.java
@@ -15,11 +15,11 @@ public class UnionFindLS {
         membres = new ArrayList<>();
     }
 
-    private EnsembleLS makeSet(int rep) throws Exception {
-
+    public EnsembleLS makeSet(int rep) throws Exception {
 
         for (EnsembleLS e : this.membres) {
-            if (e.getRepresentant() == rep) throw new Exception(rep + "déjà représentant d'un autre ensemble");
+            if (e.getRepresentant() == rep)
+                throw new Exception(rep + "déjà représentant d'un autre ensemble");
         }
 
         ListeChaineeS new_l = new ListeChaineeS(rep);
@@ -29,14 +29,14 @@ public class UnionFindLS {
         return new_e;
     }
 
-    private void union(EnsembleLS e1, EnsembleLS e2) throws Exception {
+    public void union(EnsembleLS e1, EnsembleLS e2) throws Exception {
 
         boolean trouve_e1 = false;
         boolean trouve_e2 = false;
         int index_e1 = 0;
         int index_e2 = 0;
 
-        //Determiner si les ensembles font parties de Union-Find
+        // Determiner si les ensembles font parties de Union-Find
         for (EnsembleLS e : this.membres) {
             if (e.getRepresentant() == e1.getRepresentant()) {
                 trouve_e1 = true;
@@ -50,7 +50,7 @@ public class UnionFindLS {
             throw new Exception("Ensembles non présents");
         }
 
-        //Création d'une liste issu des 2 anciennes listes
+        // Création d'une liste issu des 2 anciennes listes
         ListeChaineeS new_list = e1.getListe();
         ListeChaineeS li_temp2 = e2.getListe();
 
@@ -66,24 +66,25 @@ public class UnionFindLS {
         }
         n1.setSuivant(null);
 
-        //Détermination du représentant au hasard
+        // Détermination du représentant au hasard
         int new_rep;
-        //Random ran = new Random();
+        // Random ran = new Random();
         int nxt = LireLigne.entierAleatoire(0, 1);
-        if(nxt == 0){
+        if (nxt == 0) {
             new_rep = e1.getRepresentant();
         } else {
             new_rep = e2.getRepresentant();
         }
 
-        //Création et Ajout de l'ensemble issu de l'union des 2 précédents, suppression des anciens ensembles
+        // Création et Ajout de l'ensemble issu de l'union des 2 précédents, suppression
+        // des anciens ensembles
         EnsembleLS new_ens = new EnsembleLS(new_rep, new_list);
         this.membres.add(index_e1, new_ens);
         this.membres.remove(index_e2);
 
     }
 
-    private int find(int x) {
+    public int find(int x) {
         for (EnsembleLS e : this.membres) {
             if (e.getListe() != null) {
                 if (e.getListe().contient(x)) {
@@ -93,6 +94,5 @@ public class UnionFindLS {
         }
         return -1;
     }
-
 
 }
