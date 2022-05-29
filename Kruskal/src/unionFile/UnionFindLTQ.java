c25a79d77147e8ddfd69fd2d63bd4a698aa5196a
JeremyBrakha <jrmbrakha@gmail.com>
2022-05-29 23:10:42 +0200

    Début d'implémentation

--- Kruskal/src/unionFile/UnionFindLTQ.java
+++ Kruskal/src/unionFile/UnionFindLTQ.java
@@ -1,4 +1,12 @@
 package unionFile;
 
+import java.util.ArrayList;
+import ensemble.EnsembleLTQ;
+
 public class UnionFindLTQ {
+    private ArrayList<EnsembleLTQ> membres;
+
+    public UnionFindLTQ() {
+        membres = new ArrayList<>();
+    }
 }
