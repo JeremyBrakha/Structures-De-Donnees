d878ddaa7e00bdefc4d9a2584069f71c06132f62
JeremyBrakha <jrmbrakha@gmail.com>
2022-05-29 23:34:01 +0200

    Maj constructeur et implémentation de méthodes

--- Kruskal/src/liste/listeChaineeTeteQueue/ListeChaineeTQ.java
+++ Kruskal/src/liste/listeChaineeTeteQueue/ListeChaineeTQ.java
@@ -4,14 +4,14 @@ public class ListeChaineeTQ {
     private Noeud tete;
     private Noeud queue;
 
-    public ListeChaineeTQ(Noeud tete) {
-        this.tete = tete;
+    public ListeChaineeTQ(int tete) {
+        this.tete = new Noeud(tete);
         this.queue = null;
     }
 
-    public ListeChaineeTQ(Noeud tete, Noeud queue) {
-        this.tete = tete;
-        this.queue = queue;
+    public ListeChaineeTQ(int tete, int queue) {
+        this.tete = new Noeud(tete);
+        this.queue = new Noeud(queue);
     }
 
     public boolean teteEstVide() {
@@ -20,5 +20,34 @@ public class ListeChaineeTQ {
 
     public boolean queueEstVide() {
         return this.queue == null;
+    }
+
+    public void insererEnQueue(int x) {
+        // Cas où la liste est vide
+        if (this.teteEstVide())
+            this.tete = new Noeud(x, this);
+        else {
+            // Cas où la liste n'est pas vide, on set directement après la queue
+            this.queue.setSuivant(new Noeud(x, this));
+        }
+    }
+
+    /***
+     * Teste si x est présent dans la liste
+     *
+     * @param x : l'élément à rechercher
+     * @return true si x est présent, false sinon
+     */
+    public boolean contient(int x) {
+        boolean present = false;
+        Noeud p = this.tete;
+
+        while (p != null && !present) {
+            if (x == p.getValeur())
+                present = true;
+            p = p.getSuivant();
+        }
+
+        return present;
     }
 }
