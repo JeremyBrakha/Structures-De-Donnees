8e551737c10d2fd0e67b3758d30fa034eb6441fa
JeremyBrakha <jrmbrakha@gmail.com>
2022-05-29 12:02:59 +0200

    Main Kruskal première implémentation

--- Kruskal/src/Kruskal/KruskalLCS.java
+++ Kruskal/src/Kruskal/KruskalLCS.java
@@ -1,16 +1,33 @@
 package kruskal;
 
 import models.Graphe;
+import unionFile.UnionFindLS;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class KruskalLCS {
     public static void main(String[] args) throws Exception {
-        Graphe g = new Graphe();
+        Graphe graphePeuDense = new Graphe();
+        Graphe graphetresDense = new Graphe();
         Scanner sc = new Scanner(System.in);
         System.out.println("Ecrire un nombre (10, 100 ou 1000): ");
         int nSommets = sc.nextInt();
-        g.creerGraphTresDense(nSommets);
-        g.afficheGraphe();
+        graphePeuDense.creerGraphPeuDense(nSommets);
+        graphetresDense.creerGraphTresDense(nSommets);
+        graphePeuDense.afficheGraphe();
+        graphetresDense.afficheGraphe();
+
+        UnionFindLS ufls = new UnionFindLS();
+        List<Integer> sommetsGraphePeuDense = graphePeuDense.getSommet();
+        List<Integer> sommetsGrapheTresDense = graphetresDense.getSommet();
+        for (int sommet : sommetsGraphePeuDense) {
+            ufls.makeSet(sommet);
+        }
+
+        for (int sommet : sommetsGrapheTresDense) {
+            ufls.makeSet(sommet);
+        }
+
     }
 }
