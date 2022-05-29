43002560f84ecdeac04e70498a4a6605c3af975f
JeremyBrakha <jrmbrakha@gmail.com>
2022-05-29 23:33:39 +0200

    Implémentation de la classe

--- Kruskal/src/ensemble/EnsembleLTQ.java
+++ Kruskal/src/ensemble/EnsembleLTQ.java
@@ -1,4 +1,25 @@
 package ensemble;
 
+import liste.listeChaineeTeteQueue.ListeChaineeTQ;
+
 public class EnsembleLTQ {
+    private int representant;
+    private ListeChaineeTQ element;
+
+    public EnsembleLTQ(int r, ListeChaineeTQ l) {
+        this.representant = r;
+        this.element = l;
+    }
+
+    public int getRepresentant() {
+        return this.representant;
+    }
+
+    public void setRepresentant(int r) {
+        this.representant = r;
+    }
+
+    public ListeChaineeTQ getListe() {
+        return this.element;
+    }
 }
