2896246f737bae5c1ef9bd9bd91bb32264580d49
JeremyBrakha <jrmbrakha@gmail.com>
2022-05-30 18:56:57 +0200

    Création de la classe

--- Kruskal/src/ensemble/EnsembleUP.java
+++ Kruskal/src/ensemble/EnsembleUP.java
@@ -1,5 +1,25 @@
 package ensemble;
 
+import liste.listeChaineeUnionPonderee.ListeChaineeUP;
+
 public class EnsembleUP {
-    
+    private int representant;
+    private ListeChaineeUP element;
+
+    public EnsembleUP(int r, ListeChaineeUP l) {
+        this.representant = r;
+        this.element = l;
+    }
+
+    public int getRepresentant() {
+        return this.representant;
+    }
+
+    public void setRepresentant(int r) {
+        this.representant = r;
+    }
+
+    public ListeChaineeUP getListe() {
+        return this.element;
+    }
 }
