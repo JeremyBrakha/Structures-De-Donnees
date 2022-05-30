eded8375842097327dbe0969b7bbd3ef79227a9d
JeremyBrakha <jrmbrakha@gmail.com>
2022-05-30 18:57:20 +0200

    Création de la classe

--- Kruskal/src/liste/listeChaineeUnionPonderee/ListeChaineeUP.java
+++ Kruskal/src/liste/listeChaineeUnionPonderee/ListeChaineeUP.java
@@ -1,5 +1,64 @@
 package liste.listeChaineeUnionPonderee;
 
 public class ListeChaineeUP {
-    
+    private Noeud tete;
+    private Noeud queue;
+    private int taille;
+
+    public ListeChaineeUP(int tete) {
+        this.tete = new Noeud(tete);
+        this.taille = 1;
+        this.queue = null;
+    }
+
+    public ListeChaineeUP(int tete, int queue) {
+        this.tete = new Noeud(tete);
+        this.queue = new Noeud(queue);
+    }
+
+    public boolean teteEstVide() {
+        return this.tete == null;
+    }
+
+    public boolean queueEstVide() {
+        return this.queue == null;
+    }
+    public int getTaille() {
+        return this.taille;
+    }
+
+    public Noeud tete() {
+        return this.tete;
+    }
+
+    public void insererEnQueue(int x) {
+        // Cas où la liste est vide
+        if (this.teteEstVide()) {
+            this.tete = new Noeud(x, this);
+        } else if (this.queueEstVide()) {
+            this.queue = new Noeud(x, this);
+        } else {
+            // Cas où la liste n'est pas vide, on set directement après la queue
+            this.queue.setSuivant(new Noeud(x, this));
+        }
+    }
+
+    /***
+     * Teste si x est présent dans la liste
+     *
+     * @param x : l'élément à rechercher
+     * @return true si x est présent, false sinon
+     */
+    public boolean contient(int x) {
+        boolean present = false;
+        Noeud p = this.tete;
+
+        while (p != null && !present) {
+            if (x == p.getValeur())
+                present = true;
+            p = p.getSuivant();
+        }
+
+        return present;
+    }
 }
--- Kruskal/src/liste/listeChaineeUnionPonderee/Noeud.java
+++ Kruskal/src/liste/listeChaineeUnionPonderee/Noeud.java
@@ -1,5 +1,39 @@
 package liste.listeChaineeUnionPonderee;
 
 public class Noeud {
-    
+    private int valeur;
+    private ListeChaineeUP liste;
+    private Noeud nSuivant;
+
+    public Noeud(int valeur) {
+        this.valeur = valeur;
+        this.liste = null;
+        this.nSuivant = null;
+    }
+
+    public Noeud(int valeur, ListeChaineeUP liste, Noeud nSuivant) {
+        this.valeur = valeur;
+        this.liste = liste;
+        this.nSuivant = nSuivant;
+
+    }
+
+    public Noeud(int valeur, ListeChaineeUP liste) {
+        this.valeur = valeur;
+        this.liste = liste;
+        this.nSuivant = null;
+
+    }
+
+    public int getValeur() {
+        return this.valeur;
+    }
+
+    public Noeud getSuivant() {
+        return this.nSuivant;
+    }
+
+    public void setSuivant(Noeud nextNoeud) {
+        this.nSuivant = nextNoeud;
+    }
 }
